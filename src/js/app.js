0c1e0f4343e8a12c0ddb71e303c9ea0ee744c959
Frederick Lopez <mtester010@gmail.com>
2022-05-02 11:31:15 -0500

    Add the schedule functionality

--- src/js/app.js
+++ src/js/app.js
@@ -7,35 +7,37 @@ function getStreets(userSearch) {
 function generateStreetsHTML(streets) {
   const streetsContainer = document.querySelector('.streets')
   streetsContainer.textContent = '';
-  streets.forEach(street => {
-    streetsContainer.insertAdjacentHTML(
-      'beforeend',
-      `
-      <a href="#" data-street-key="${street.key}">${street.name}</a>
-      `
-    )
-  })
+  if (typeof streets !== 'object') {
+    streetsContainer.textContent = streets;
+  } else {
+    streets.forEach(street => {
+      streetsContainer.insertAdjacentHTML(
+        'beforeend',
+        `
+        <a href="#" data-street-key="${street.key}">${street.name}</a>
+        `
+      )
+    })
+  }
 }
 
-function generateStopsHTML(stops, streetNameEl) {
+function generateStopsHTML(data, streetNameEl) {
+  const { stopName, crossStreet, direction, busNum, time } = data;
   const streetName = document.querySelector('#street-name')
-  const stopTable = document.querySelector('tbody')
+  const stopTable = document.querySelector('tbody');
   streetName.textContent = `Displaying Result for ${streetNameEl}`
-
-  stops.forEach(stop => {
-    stopTable.insertAdjacentHTML(
-      'beforebegin',
-      `
-      <tr>
-        <td>${stop.name}</td>
-        <td>${stop[`cross-street`].name}</td>
-        <td>${stop.direction}</td>
-        <td>650</td>
-        <td>09:30 AM</td>
-      </tr>
-      `
-    )
-  })
+  stopTable.insertAdjacentHTML(
+    'beforebegin',
+    `
+    <tr>
+      <td>${stopName}</td>
+      <td>${crossStreet}</td>
+      <td>${direction}</td>
+      <td>${busNum}</td>
+      <td>${time}</td>
+    </tr>
+    `
+  )
 }
 
 function getStops(stopID) {
@@ -44,18 +46,64 @@ function getStops(stopID) {
     .then((data) => data.stops)
 }
 
-function searchHandler() {
+//get stop info and route schedules for a given stop key
+function getSchedule(stopKey) {
+  const url = `https://api.winnipegtransit.com/v3/stops/${stopKey}/schedule.json?api-key=tkJgIR5_6mI5h3wthGp8&max-results-per-route=2`
+  //`${BASE_URL}/stops/${stopkey}
+  return fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      return {
+        'stop-name': data['stop-schedule'].stop.name,
+        'cross-street': data['stop-schedule'].stop['cross-street'].name,
+        direction: data['stop-schedule'].stop.direction,
+        'route-schedules': data['stop-schedule']['route-schedules']
+      };
+    });
+};
+
+function searchHandler(e) {
   const userSearch = document.querySelector('#search-panel').value
   console.log(userSearch);
+  e.preventDefault();
   getStreets(userSearch)
     .then((streets) => generateStreetsHTML(streets))
 }
 
 function clickHandler(e) {
   const streetId = e.target.dataset.streetKey;
-  const streetNameEl = e.target.textContent
+  const streetNameEl = e.target.textContent;
   getStops(streetId)
-  .then((stops) => generateStopsHTML(stops, streetNameEl))
+    .then((stops) => {
+      //loop through each response
+      // for each stop:
+      // call get schedule with stopkey
+      // return stop and schedule data
+      // loop through schedule data
+      // render the table row for each time
+      // generateStopsHTML(stops, streetNameEl)
+      stops.forEach((stop) => {
+        getSchedule(stop.key)
+          .then((schedule) => {
+            schedule['route-schedules'].forEach((route) => {
+              route['scheduled-stops'].forEach(time => {
+                generateStopsHTML({
+                  stopName: stop.name,
+                  crossStreet: stop['cross-street'].name,
+                  direction: stop.direction,
+                  busNum: route.route.number,
+                  time: formatDate(time.times.departure.scheduled),
+                }, streetNameEl)
+              })
+            })
+          })
+      })
+    })
+}
+
+function formatDate(date) {
+  const rawDate = new Date(date);
+  return rawDate.toLocaleTimeString('ca-EN');
 }
 
 document
@@ -69,3 +117,5 @@ document
 //The scheduled arrival time of the next 2 buses for each route at this stop.
 // The route number(bus #) for each of these buses.
 // The name of the street (this is displayed ABOVE the table)
+//Note
+// Check the maps api with geographic data on winnipeg transit
